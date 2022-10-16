ca42af017a6992cade1f2438ff450aba90aa9c8d
29-GlennTho <114901634+27-GlennTho@users.noreply.github.com>
2022-10-17 04:24:25 +0800

    Scripts Implemented
    Script made for score, water collision and timer script

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreScript : MonoBehaviour
+{
+    public float score;
+    public Text pointText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointText.text = "Score: "+score;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Coin")
+        {
+            score += 10;
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class TimerScript : MonoBehaviour
+{
+    private float scoreValue;
+    public float totalCoins;
+    public float timeLeft;
+    public int timeRemaining;
+
+    public Text ScoreText;
+    public Text TimerText;
+
+    private float timerValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timeRemaining = Mathf.FloorToInt(timeLeft % 60);
+        TimerText.text = "Timer: " + timeRemaining.ToString();
+        if(scoreValue==totalCoins)
+        {
+            if(timeLeft<=timerValue)
+            {
+                SceneManager.LoadScene("GameLose");
+            }
+        }
+    }
+}
